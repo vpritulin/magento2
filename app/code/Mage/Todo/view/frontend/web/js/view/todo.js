c184e6a610569134b9e3e8a76c412cb9fc34b4eb
Vladislav <vpritulin@gomage.com>
2022-12-25 14:13:40 +0200

    add js event

--- app/code/Mage/Todo/view/frontend/web/js/view/todo.js
+++ app/code/Mage/Todo/view/frontend/web/js/view/todo.js
@@ -1,6 +1,34 @@
 define([
-    'uiComponent'
-], function (Component){
+    'uiComponent',
+    'jquery'
+], function (Component, $) {
     'use strict';
-    return Component.extend({});
+
+    return Component.extend({
+        defaults: {
+            tasks: [
+                {id:1, label: "Task 1", status:false},
+                {id:2, label: "Task 2", status:false},
+                {id:3, label: "Task 3", status:false},
+                {id:4, label: "Task 4", status:true}
+            ]
+        },
+        initObservable: function () {
+            this._super().observe(['tasks']);
+
+            return this;
+        },
+
+        switchStatus:function (data,event) {
+            const taskId = $(event.target).data('id');
+
+            let items = this.tasks.map(function (task){
+               if (task.id === taskId){
+                   task.status =!task.status;
+               }
+               return task;
+            });
+            this.tasks(items);
+        }
+    });
 });
